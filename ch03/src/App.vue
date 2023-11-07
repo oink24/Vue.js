434b7b0d05462630b2e24895b77d4f979fb09476
oink24 <oooq3o@gmail.com>
2023-11-07 10:08:59 +0900

    Vue.js 컴포넌트 실습

---
+++ ch03/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import MyComponent1 from "./components/MyComponent1.vue";
+import MyComponent2 from "./components/MyComponent2.vue";
+import MyComponent3 from "./components/MyComponent3.vue";
+</script>
+
+<template>
+  <h3>ch03. 컴포넌트</h3>
+  <h4>1) 컴포넌트</h4>
+  <MyComponent1 />
+  <MyComponent2 />
+  <MyComponent3 />
+
+  <h4>2) Options API</h4>
+  <h4>3) 컴포넌트 통신</h4>
+  <h4>4) 컴포넌트 생명주기</h4>
+</template>
+
+<style scoped></style>
---
+++ ch03/src/components/MyComponent1.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "MyComponent1", // 생략가능
+};
+</script>
+
+<template>
+  <h4>MyComponent1</h4>
+  <p>사용자 정의 컴포넌트1</p>
+</template>
+
+<style scoped>
+h4 {
+  color: skyblue;
+}
+</style>
