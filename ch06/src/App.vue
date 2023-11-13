81d1fd7caf3fc0765e2c32058b9ba73cfba7d7bb
oink24 <oooq3o@gmail.com>
2023-11-13 11:34:51 +0900

    Vue.js Pinia 실습

---
+++ ch06/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>ch06. pinia 실습</h1>
+
+  <p>
+    mainStore title : {{ mainStore.title }}<br />
+    mainStore count : {{ mainStore.count }}<br />
+  </p>
+  <p>cities : {{ mainStore.cities }}<br /></p>
+  <table border="1">
+    <tr>
+      <th>아이디</th>
+      <th>이름</th>
+      <th>나이</th>
+    </tr>
+    <tr v-for="person in mainStore.persons">
+      <td>{{ person.uid }}</td>
+      <td>{{ person.name }}</td>
+      <td>{{ person.age }}</td>
+    </tr>
+  </table>
+
+  <hr />
+  <MyComponent1 />
+</template>
+
+<script setup>
+import { useMainStore } from "./store/mainStore";
+import MyComponent1 from "./components/MyComponent1.vue";
+
+const mainStore = useMainStore();
+</script>
+
+<style scoped></style>
